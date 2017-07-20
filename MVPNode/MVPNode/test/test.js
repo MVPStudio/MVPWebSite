ba87b07ccbe7ed3662a2625b1cd4c036f0782d41
Joshua Evans <TheJoshuaEvans@gmail.com>
2017-07-19 20:14:30 -0700

    Improved SQL Connection - There is no longer a default config file, that file must be provided manually - Updated gitignore to reflect config file changes - renamed the `dbConnect` file to `mssql_connect` in order to be more specific - Reworked mssql_connect module to require construction of an instance when importing. This allows a callback to be provided to the constructor - Added stubs for planned functionality to mssql_connect - Improved test so it actually detects connection errors - Added `sql` dependency

--- MVPNode/MVPNode/test/test.js
+++ MVPNode/MVPNode/test/test.js
@@ -1,9 +1,24 @@
 var assert = require('assert');
 var mssqlConnect;
-describe('dbConnect', function() {
-  describe('Initialization', function() {
-    it('should initialize without errors', function() {
-      mssqlConnect = require('../utils/dbConnect.js').MSSQLConnect;
+describe('dbConnect', function () {
+
+    describe('Initialization', function () {
+
+      it('should initialize without errors', function (done) {
+        
+        // Create a new MSSQLConnect object and initialize it. The object will automatically attempt to connect with the
+        // database when the constructor is called
+        var MSSQLConnect = require('../utils/mssql_connect.js').MSSQLConnect;
+
+        // Create a new mssqlConnect instance. The constructor automatically attempts to connect to the database and will
+        // make an err callback if the connection fails
+        var mssqlConnect = new MSSQLConnect(function (err) {
+            if (err) {
+                done(err);
+            } else {
+                done();
+            }
+          });
+      });
     });
-  });
 });
